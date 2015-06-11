af94c6ca197c38bde0617c44811ee64ee1b9a7c2
domarps <spmd92@gmail.com>
2015-06-11 09:19:38 +0530

    modified maximiseSum

--- maximiseSum.cpp
+++ maximiseSum.cpp
@@ -14,7 +14,8 @@ int main()
     while(T--)
         {
             long long N,S,M,temp,result,sum=0;
-            vector<int> arr;
+            vector<long long> arr;
+	    vector<long long> prefixSum;
             cin >> N >> M;
             S = N;
             while(S--)    
@@ -22,13 +23,14 @@ int main()
                 cin >> temp;
                 arr.push_back(temp); 
             }
-            sum    = arr[0];
-            result = sum;
-            for(int i = 1; i < N ; i++) 
+            prefixSum.push_back(arr[0]);
+            result     = prefixSum[0];
+            for(long long i = 1; i < N ; i++) 
             {
-                 sum = sum + (arr[i]);
-                 sum = sum  %  M;
-                 result = max(sum,result);
+                 sum = prefixSum[i-1] + (arr[i]);
+		 sum = sum % M;
+                 prefixSum.push_back(sum);
+                 result = max(prefixSum[i],result);
             }  
             cout << result << endl;
         }
